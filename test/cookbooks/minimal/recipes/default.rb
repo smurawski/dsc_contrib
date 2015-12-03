812c560f6b3612de89260a15fe9614f655a02b30
Steven Murawski <steven.murawski@gmail.com>
2015-12-03 13:14:00 -0600

    last tweaks for basic integration test

--- test/cookbooks/minimal/recipes/default.rb
+++ test/cookbooks/minimal/recipes/default.rb
@@ -1,5 +1,15 @@
 #
 
+unless Chef::Platform.supports_dsc_invoke_resource?(node)
+  include_recipe 'powershell::powershell5'
+
+  reboot 'Now' do
+    action :reboot_now
+    guard_interpreter :powershell_script
+    not_if '$psversiontable.psversion.major -ge 5'
+  end
+end
+
 include_recipe 'dsc_contrib::dsc_resource_lcm_setup'
 
 dsc_resource 'Install IIS' do
@@ -9,12 +19,12 @@ dsc_resource 'Install IIS' do
   reboot_action :request_reboot
 end
 
-# If installing didn't require a reboot, 
+# If installing didn't require a reboot,
 # then removing it immediately after will.
 dsc_resource 'Remove IIS' do
   resource :windowsfeature
   property :name, 'web-server'
-  property :ensure, 'Present'
+  property :ensure, 'Absent'
   reboot_action :request_reboot
 end
 
