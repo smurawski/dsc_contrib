c5e0f1503b5dc9a2be32142181631a4ca2f0048c
Steven Murawski <steven.murawski@gmail.com>
2016-04-13 09:33:20 -0500

    fix dependency constraint

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'smurawski@chef.io'
 license 'apache2'
 description 'Helpers and previews of DSC functionality'
 long_description 'Helpers and previews of DSC functionality'
-version '0.4.0'
+version '0.4.1'
 
 source_url 'https://github.com/smurawski/dsc_contrib' if respond_to?(:source_url)
 issues_url 'https://github.com/smurawski/dsc_contrib/issues' if respond_to?(:issues_url)
 
-depends 'compat_resource', '>=12.5'
+depends 'compat_resource', '>= 12.5'
