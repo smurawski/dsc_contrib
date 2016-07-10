6cfdfe2604725757a238d2f10de32003765acb1e
Steven Murawski <steven.murawski@gmail.com>
2016-07-10 08:57:34 -0500

    ps_module_spec helper

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,11 @@ maintainer_email 'smurawski@chef.io'
 license 'apache2'
 description 'Helpers and previews of DSC functionality'
 long_description 'Helpers and previews of DSC functionality'
-version '0.4.1'
+version '0.5.0'
 
 source_url 'https://github.com/smurawski/dsc_contrib' if respond_to?(:source_url)
 issues_url 'https://github.com/smurawski/dsc_contrib/issues' if respond_to?(:issues_url)
 
 depends 'compat_resource', '>= 12.5'
+
+gem 'dsc_lcm_configuration'
